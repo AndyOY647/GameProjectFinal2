19c5a5c513f9409b1f7bbd3bf6da210dbaa4996d
AndyOY647 <60327435+AndyOY647@users.noreply.github.com>
2023-12-16 22:54:56 -0500

    Health, chest, and map
    Map collision is still off by a little bit

--- playerFunctions.js
+++ playerFunctions.js
@@ -1,47 +1,19 @@
 // movement and camera control
 
 function movement(player,speed, window){
-    let leftWall = 160;
-    let rightWall = window.width-160;
-    let top =  -6522;
-    let bottom =  860;
     let attack = false;
     let orginial_width = 120;
-    let attackCounter = 0;
+    let opened = false;
     camera.on();
     if(keyIsPressed){
         if (kb.pressing('left')) {
             player.vel.x = -speed;
             player.changeAni('walk');
-            //boarder detection
-            //left collision
-            if(player.y < -570 && player.y > -5280){
-              leftWall = 452;
-            }else if(player.y < -5280){
-              leftWall = player.width;
-            }else{
-              leftWall = 160;
-            }
-            if(player.x < leftWall){
-              player.vel.x = 0;
-            }
-            
           }
           else if (kb.pressing('right')) {
             player.vel.x = speed;
             player.changeAni('walk');
-            //boarder detection
-            //right collision
-            if(player.y < -570 && player.y > -5295){
-              rightWall = windowWidth - 452;
-            }else if(player.y < -5280){
-              rightWall = windowWidth - player.width;
-            }else{
-              rightWall = windowWidth - 160;
-            }
-            if(player.x > rightWall){
-              player.vel.x = 0;
-            }
+           
         } 
         else if (kb.pressing('up')) {
             player.vel.y = -speed;
@@ -59,17 +31,6 @@ function movement(player,speed, window){
             }
             else{
               player.vel.x = 0;
-            }
-            if(player.x > windowWidth-480 && player.x <= windowWidth-130 && player.y < -530 && player.y > -600){
-              top = -535;
-            }
-            if(player.x > 0 && player.x <= 465 && player.y < -530 && player.y > -600){
-              top = -535;
-            }
-            //boarder detection
-            //top of the map
-            if(player.y < top){
-              player.vel.y = 0;
             }
         } 
         else if (kb.pressing('down')) {
@@ -90,21 +51,8 @@ function movement(player,speed, window){
             else{
               player.vel.x = 0;
             }
-            //boarder detection
-            //bottom of the map
-            if(player.x > windowWidth-480 && player.x <= windowWidth && player.y == -5300){
-              bottom = -5305;
-            }
-            if(player.x > 0 && player.x <= 465 && player.y == -5300){
-              rect(465, -5200, 100,5)
-              bottom = -5305;
-            }
-            if(player.y > bottom){
-              player.vel.y = 0;
-              
-            }
           }
-        else if (kb.presses(' ') && attack == false ) {
+        else if (kb.pressing(' ') && attack == false ) {
           attack = true;
           player.changeAni('attack');
           player.vel.x = 0;
@@ -112,9 +60,6 @@ function movement(player,speed, window){
           if(attack){
             player.width = orginial_width+5;
           }
-         
-          
-          
       }
       }else if(!keyIsPressed){
         // player.removeColliders();
@@ -123,10 +68,30 @@ function movement(player,speed, window){
         player.changeAni('idle');
         player.vel.x = 0;
         player.vel.y = 0;
-        // if(!attack){
-        //   player.removeColliders();
-        // }
-        
       }
     camera.y = player.y;
 }
+
+function checkInteract(player, chest, potion){ // add creep and boss to the attribute for interactivity later
+
+  if(player.collide(chest)){
+    if(chest.image == chestClosed){
+      fill(255);
+      textSize(25);
+      text('press ' + "'z' " + 'to open chest', chest.x-20, chest.y-100);
+      if(kb.presses('z')){
+        chest.image = chestOpen;
+        potion.visible  = true;
+          for(let i = 0; i < 30; i+=5){
+            potion.y -=i;
+            print(i);
+            // 
+            
+        }
+      }
+    }
+  }
+
+}
+
+
--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ let percy;
 let percyWidth = 120;
 let percyHeight = 320;
 let standLoad, walkingLoad, attackLoad;
-let speed = 5;
+let speed = 10;
+let health = [];
 
 //enemy variables
 let wolf;
@@ -11,6 +12,8 @@ let wolfAttackLoad, wolfWalkLoad;
 
 //screen and background
 let startScreen, bg;
+
+let chest;
 
 function preload(){
   standLoad = loadAni(
@@ -51,7 +54,16 @@ function preload(){
 
   startScreen = loadImage('medp370final_startscreen.png');
   bg = loadImage('medp370finalgame_background.png');
-  health = loadImage('medp370_final_assets/health_heart.png');
+  bgTop = loadImage('medp370_final_assets/top.png');
+  leftWall = loadImage('medp370_final_assets/left wall.png');
+  rightWall = loadImage('medp370_final_assets/right_wall.png');
+  heart = loadImage('medp370_final_assets/health_heart.png');
+  emptyHeart = loadImage('medp370_final_assets/empty_heart.png');
+  chestClosed = loadImage('medp370_final_assets/itemchest_closed.png');
+  chestOpen = loadImage('medp370_final_assets/itemchest_opened.png');
+  potionImage = loadImage('medp370_final_assets/potion_item.png');
+
+
 
 } // preload end
 
@@ -62,6 +74,32 @@ let win;
 function setup() {
   win = new Canvas(windowWidth, windowHeight);
   percy = new Sprite(windowWidth/2, -5000, percyWidth, percyHeight);
+  
+  wallL = new Sprite(leftWall.width/2, -leftWall.height/2.55, leftWall.width, leftWall.height, 'static');
+  wallL.image = leftWall;
+  wallR = new Sprite( windowWidth - rightWall.width/2, -rightWall.height/2.55, rightWall.width, rightWall.height, 'static');
+  wallR.image = rightWall;
+  wallT = new Sprite(windowWidth/2, -6870, bgTop.width, bgTop.height, 'static');
+  wallT.image = bgTop;
+  wallB = new Sprite(windowWidth/2,windowHeight-300, windowWidth, 20, 'static');
+
+  for(let i = 0; i <  3; i++){
+    health[i] = new Sprite((i+1)*100,50, 90, 80, 'kenetic');
+    health[i].image = heart;
+  }
+
+  chest = new Sprite(640, -4820, chestClosed.width, chestClosed.height, 'static');
+  chest.image = chestClosed;
+  chest.scale = 0.5;
+
+  potion = new Sprite(chest.x, chest.y, potionImage.width, potionImage.height, 'n');
+  potion.image = potionImage;
+  potion.scale = 0.2;
+
+
+ 
+  
+
   // s
   percy.scale= 0.5;
   percy.rotationLock = true;
@@ -78,29 +116,64 @@ function setup() {
   percy.changeAni('idle');
   percy.visible = false;
   wolf.visible = false;
+  for(let i = 0; i <  3; i++){
+    health[i].visible = false;
+  }
+  wallL.visible = false;
+  wallR.visible = false;
+  wallB.visible = false;
+  bgTop.visible = false;
 }
 
 
 function draw() {
  
   camera.on();
-  percy.debug = true;
-  wolf.debug = true;
-  percy.pixelPerfect = true;
+  
+  // percy.debug = true;
+  // wolf.debug = true;
+  // percy.pixelPerfect = true;
+  // wallL.debug = true;
+  // wallR.debug = true;
+  // wallT.debug = false;
+  // chest.debug = false;
+  
   
   image(startScreen, 0, 0, win.width, win.height);
   if(kb.presses('enter')){
     run = true;
     percy.visible = true;
     wolf.visible = true;
+    wallL.visible = false;
+    wallR.visible = false;
+    potion.visible = false;
+   
+    
   }
 
 
   if(run){
-    image(bg, 0,-bg.height+canvas.height);
+    background(255)
+    
+    // image(bgTop, 0, -6800);
+    // image(leftWall, 0, -leftWall.height+ windowHeight/1.5);
+    // image(rightWall, windowWidth - rightWall.width, -rightWall.height  + windowHeight/1.5);
+    image(bg, 0,-bg.height+windowHeight/1.5);
+    
+   
     movement(percy,speed, win);
+    for(let i = 0; i <  3; i++){
+      health[i].visible = true;
+      health[i].y = camera.y-400;
+    }
+    checkInteract(percy, chest, potion);
+    
+   
+    
   }
-  console.log(percy.x, percy.y);
+  // console.log(percy.x, percy.y);
+  console.log(chest.x, chest.height- 100)
+  
   
 }  
 
